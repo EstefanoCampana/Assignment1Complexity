3dfc19bf7c521272c3b1de8c9c9aefb84210b558
TerrellAW <terraw1999@proton.me>
2025-10-11 17:59:44 -0600

    Fix: Insertion sort compliance
    Insertion Sort now complies with assignment instructions.

--- src/utilities/InsertionSort.java
+++ src/utilities/InsertionSort.java
@@ -1,36 +1,60 @@
 package utilities;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 
 /**
  * Class for handling insertion sort.
- * 
+ *
  * @author TerrellAW
  * @version 09-10-2025
  */
 public class InsertionSort {
-	/**
-	 * Sorts a list of objects using insertion sort.
-	 * Sorts based on the chosen comparator's compare method.
-	 * 
-	 * @author TerrellAW
-	 * @param objects ArrayList of generic objects
-	 * @param comparator Chosen comparator for comparing objects
-	 */
-	public static <T> void insertionSort(ArrayList<T> objects, Comparator<T> comparator) {
-		
-		int len = objects.size();
-		
-		for (int i = 1; i < len; ++i) {
-			T temp = objects.get(i);
-			int j = i - 1;
-			
-			while (j >= 0 && comparator.compare(objects.get(j), temp) >= 0) {
-				objects.set(j + 1, objects.get(j));
-				j = j - 1;
-			}
-			objects.set(j + 1, temp);
-		}
-	}
+
+    /**
+     *
+     */
+    public static <T extends Comparable<T>> void insertionSort(
+        T[] unsortedArray
+    ) {
+        int len = unsortedArray.length;
+
+        for (int i = 1; i < len; ++i) {
+            T temp = unsortedArray[i];
+            int j = i - 1;
+
+            while (
+                j >= 0 && ((Comparable<T>) unsortedArray[j]).compareTo(temp) < 0
+            ) {
+                unsortedArray[j] = unsortedArray[j + 1];
+                j = j - 1;
+            }
+            unsortedArray[j + 1] = temp;
+        }
+    }
+
+    /**
+     * Sorts a list of objects using insertion sort.
+     * Sorts based on the chosen comparator's compare method.
+     *
+     * @author TerrellAW
+     * @param objects ArrayList of generic objects
+     * @param comparator Chosen comparator for comparing objects
+     */
+    public static <T> void insertionSort(
+        T[] unsortedArray,
+        Comparator<T> comparator
+    ) {
+        int len = unsortedArray.length;
+
+        for (int i = 1; i < len; ++i) {
+            T temp = unsortedArray[i];
+            int j = i - 1;
+
+            while (j >= 0 && comparator.compare(unsortedArray[j], temp) < 0) {
+                unsortedArray[j] = unsortedArray[j + 1];
+                j = j - 1;
+            }
+            unsortedArray[j + 1] = temp;
+        }
+    }
 }
