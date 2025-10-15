319bb71c52f23fd0dad6a54aaf7313cbd37fc3e1
TerrellAW <terraw1999@proton.me>
2025-10-14 19:28:55 -0600

    Fix: Argument parsing method
    If conditional for ensuring argument length was valid was in the wrong place.

--- src/appDomain/AppDriver.java
+++ src/appDomain/AppDriver.java
@@ -22,15 +22,15 @@ import utilities.SelectionSort;
 /**
  * <p>
  * This application driver code is designed to be used as a basis for the
- * Complexity and Sorting assignment that will be developed in the CPRG304 
+ * Complexity and Sorting assignment that will be developed in the CPRG304
  * F2025 class at SAIT. The implementors of this applications will be required
  * to add all the correct functionality.
  * </p>
  */
 public class AppDriver
 {
-	
-	
+
+
 	public static ArrayList<ShapeObject> readFile()
 	{
 		File shapesData = new File("res/shapes1.txt");
@@ -52,47 +52,47 @@ public class AppDriver
 		{
 			String[] attributes;
 			String shapeName;
-			
+
 			//Creates new null superclass object to use it as base constructor for each subclass.
 			ShapeObject shape = null;
-			
+
 			String itemNumber = content.nextLine();
-			
+
 			attributes = itemNumber.trim().split("\\s+");
 
 			shapeName = attributes[0];
 
-			if(shapeName.equals("Cone")) 
+			if(shapeName.equals("Cone"))
 			{
 				shape = new Cone(Double.parseDouble(attributes[1]), Double.parseDouble(attributes[2]));
 			}
-			else if(shapeName.equals("Cylinder")) 
+			else if(shapeName.equals("Cylinder"))
 			{
 				shape = new Cylinder(Double.parseDouble(attributes[1]), Double.parseDouble(attributes[2]));
 			}
-			else if(shapeName.equals("Pyramid")) 
+			else if(shapeName.equals("Pyramid"))
 			{
 				shape = new Pyramid(Double.parseDouble(attributes[1]), Double.parseDouble(attributes[2]));
 			}
-			else if(shapeName.equals("SquarePrism")) 
+			else if(shapeName.equals("SquarePrism"))
 			{
 				shape = new SquarePrism(Double.parseDouble(attributes[1]), Double.parseDouble(attributes[2]));
 			}
-			else if(shapeName.equals("TriangularPrism")) 
+			else if(shapeName.equals("TriangularPrism"))
 			{
 				shape = new TriangularPrism(Double.parseDouble(attributes[1]), Double.parseDouble(attributes[2]));
 			}
-			else if(shapeName.equals("PentagonalPrism")) 
+			else if(shapeName.equals("PentagonalPrism"))
 			{
 				shape = new PentagonalPrism(Double.parseDouble(attributes[1]), Double.parseDouble(attributes[2]));
 			}
-			else if(shapeName.equals("OctagonalPrism")) 
+			else if(shapeName.equals("OctagonalPrism"))
 			{
 				shape = new OctagonalPrism(Double.parseDouble(attributes[1]), Double.parseDouble(attributes[2]));
 			}
             //Adds the appliance to the list.
 			shapeObjects.add(shape);
-			
+
 		}
 		return shapeObjects;
 	}
@@ -105,14 +105,14 @@ public class AppDriver
 	 * @param args Command line arguments taken from main.
 	 */
 	public static void argSwitch(String[] args, Config config) {
-	 	if (arg.length() < 2) {
-			continue;
-		}
-
 		for (String arg : args) {
+    	 	if (arg.length() < 2) {
+    			continue;
+    		}
+
 			String flag = arg.substring(0, 2);
 			char value = arg.charAt(2);
-			
+
 			switch (flag.toLowerCase()) {
 			case "-f":
 				config.setFilePath(arg.substring(2));
@@ -128,10 +128,10 @@ public class AppDriver
 			}
 		}
 	}
-	
+
 	/**
 	 *  The main method is the entry point of the application.
-	 *  
+	 *
 	 *  @param args The input to control the execution of the application.
 	 */
 	public static void main( String[] args )
@@ -158,24 +158,24 @@ public class AppDriver
 		Object[] arrayObj = readFile().toArray();
 		ShapeObject[] arrayObj2 = Arrays.copyOf(arrayObj, arrayObj.length, ShapeObject[].class);
 		MergeSort.mergeSort(arrayObj2, 0, arrayObj2.length - 1);
-		for(int x = 0; x < arrayObj2.length ; x++) 
+		for(int x = 0; x < arrayObj2.length ; x++)
 		{
 			System.out.println(arrayObj2[x]);
 		}
 		System.out.println("");
-		
+
 		VolumeCompare volComp = new VolumeCompare();
 		AreaCompare areaComp = new AreaCompare();
 		MergeSort.mergeSort(arrayObj2, 0, arrayObj2.length - 1, volComp);
-		for(int x = 0; x < arrayObj2.length ; x++) 
+		for(int x = 0; x < arrayObj2.length ; x++)
 		{
 			System.out.println(arrayObj2[x]);
 		}
 		System.out.println("");
 		VolumeCompare.isUsed = false;
-		
+
 		SelectionSort.selectionSort(arrayObj2);
-		for(int x = 0; x < arrayObj2.length ; x++) 
+		for(int x = 0; x < arrayObj2.length ; x++)
 		{
 			System.out.println(arrayObj2[x]);
 		}
@@ -183,14 +183,14 @@ public class AppDriver
 		AreaCompare.isUsed = false;
 
 		SelectionSort.selectionSort(arrayObj2, areaComp);
-		for(int x = 0; x < arrayObj2.length ; x++) 
+		for(int x = 0; x < arrayObj2.length ; x++)
 		{
 			System.out.println(arrayObj2[x]);
 		}
 		System.out.println("");
 
 		QuickSort.quickSort(arrayObj2, 0, arrayObj2.length - 1);
-		for(int x = 0; x < arrayObj2.length ; x++) 
+		for(int x = 0; x < arrayObj2.length ; x++)
 		{
 			System.out.println(arrayObj2[x]);
 		}
