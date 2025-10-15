a8f84032a6bec79264d517b6b8574c0d7f23cb0e
TerrellAW <terraw1999@proton.me>
2025-10-14 18:54:13 -0600

    Add: Config class
    Handles program configurations, such as file path and type of sorting method used.

---
+++ src/appDomain/Config.java
@@ -0,0 +1,104 @@
+package appDomain;
+
+/**
+ * Contains fields and methods for configuring the program with command line arguments.
+ *
+ * @author TerrellAW
+ * @version 13-10-2025
+ */
+public class Config {
+	private String filePath;
+	
+	/**
+	 * Enumerates possible values that can be used to compare objects.
+	 *
+	 * @author TerrellAW
+	 * @version 13-10-2025
+	 */
+	private enum CompareType {
+		VOLUME('v'),
+		HEIGHT('h'),
+		BASE_AREA('a');
+
+		private final char compArg;
+
+		public char getCompArg() {
+			return compArg;
+		}
+		
+		CompareType(char compArg) {
+			this.compArg = compArg;
+		}
+
+		public static CompareType fromChar(char c) {
+			for (CompareType type : values()) {
+				if (type.compArg == c) {
+					return type;
+				}
+			}
+			throw new IllegalArgumentException("Invalid compare type: " + c);
+		}
+	}
+
+	/**
+	 * Enumerates possible sort types that can be used to sort objects.
+	 *
+	 * @author TerrellAW
+	 * @version 13-10-2025
+	 */
+	private enum SortType {
+		BUBBLE('b'),
+		SELECTION('s'),
+		INSERTION('i'),
+		MERGE('m'),
+		QUICK('q'),
+		CHOICE('z');
+
+		private final char sortArg;
+
+		public char getSortArg() {
+			return sortArg;
+		}
+
+		SortType(char sortArg) {
+			this.sortArg = sortArg;
+		}
+
+		public static SortType fromChar(char c) {
+			for (SortType type : values()) {
+				if (type.sortArg == c) {
+					return type;
+				}
+			}
+			throw new IllegalArgumentException("Invalid sort type: " + c);
+		}
+	}
+
+	// Setters
+	
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public void setCompareType(char type) {
+		this.compareType = CompareType.fromChar(type);
+	}
+
+	public void setSortType(char type) {
+		this.sortType = SortType.fromChar(type);
+	}
+
+	// Getters
+	
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public CompareType getCompareType() {
+		return compareType;
+	}
+
+	public SortType getSortType() {
+		return sortType;
+	}
+}
